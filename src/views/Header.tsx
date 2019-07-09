bc844a3477def0d0804cef54a3e376244c2a2246
lrypto <lloydrebello@gmail.com>
2019-07-09 18:35:04 -0400

    logo added

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Logo from "./../img/esemble6.png";
 
 export default function Header() {
   return (
     <React.Fragment>
        <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-          <a className="navbar-brand" href="/"><p className="logo-text">esemble</p></a>
+          <a className="navbar-brand" href="/"><p className="logo-text"><img className="logo" src={Logo}/></p></a>
           <button className="navbar-toggler" type="button" data-toggle="collapse"
             data-target="#navbarResponsive">
               <span className="navbar-toggler-icon"></span>
