45e46d04d77d8ff9752fbf76ec2c943ffc794e19
lrypto <lloydrebello@gmail.com>
2019-07-09 17:51:21 -0400

    api test component for json rpc poc

--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -1,36 +1,12 @@
 import React from 'react'
 import Web3Provider from 'web3-react';
 import useWeb3Config from '../components/chainstate/useWeb3Config';
-import EventTest from '../components/test/EventTest';
-import ShowUserAccount from '../components/upiko/users/ShowUserAccount';
-
-/*
- this.contract.addEventListener((v) => {
-      this.setState({ skillsCount: v._value })
-    })
-  }
-  
-    this.upikoAppInstance.events.SkillAdded((err, event) => {
-      if (err) console.error('Error on event', err)
-      else {
-        if (this.onEvent) {
-          this.onEvent(event.returnValues)
-        }
-      }
-    });
-
-  addEventListener(fn) {
-    this.onEvent = fn
-  }
-
-
-*/
+import ApiTest from '../components/test/ApiTest';
 
 
 export default function Test() {
   const web3Config = useWeb3Config();
-  
-  //useEventListener('mousemove', handler);
+ 
 
   return (
     <div className="offset">
@@ -40,7 +16,7 @@ export default function Test() {
             <h3 className="heading text-center">==--* test  +___=-`</h3>
             <div className="heading-underline"></div>
             <Web3Provider connectors={web3Config.connectors} libraryName={web3Config.libraryName} web3Api={web3Config.web3Api}> 
-            
+              <ApiTest />
             </Web3Provider>
           </div>
         </div>
