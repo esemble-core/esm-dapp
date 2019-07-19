6b86e063030083305162c5a7653c9f0f67e8ef01
lrypto <lloydrebello@gmail.com>
2019-07-19 14:51:46 -0400

    added consumption of the project api with_tasks option

--- src/components/esm/projects/ShowProject.tsx
+++ src/components/esm/projects/ShowProject.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import Axios from 'axios';
+import { notifyError } from '../../../common/Actions';
+import { Card } from 'antd';
 
 export default function ShowProject(props:any) {
   const [project, setProject] = React.useState({});
+  const [tasks, setTasks] = React.useState({});
+
 
   React.useEffect(() => {
     const loadProj = async() => {
       let url = new String(window.location);
       let projectId = url.split("/").pop();
-      
+//      get('/api/v1/projects/1', params: {with_tasks: 'true'})
+
+      try{
+        const result = await Axios(
+          `/api/v1/projects/${projectId}`, {params: {with_tasks: true}},
+        );
+        console.log("result(ShowProject)", result.data)
+        setProject(result.data.data);
+        setTasks(result.data.include.tasks);
+        }catch (error){
+          console.error("Could not connect to server");
+          notifyError("Could not connect to server, please try again later");
+        }
     }
     loadProj();
   }, [])
@@ -15,8 +32,11 @@ export default function ShowProject(props:any) {
   
 
   return (
-    <div>
-      ShowProject
+    <div style={{ background: '#ECECEC', padding: '30px' }}>
+        <Card title="Project" bordered={false} >
+         <p>Project{JSON.stringify(project)}</p>
+          <p>Tasks{JSON.stringify(tasks)}</p>
+       </Card>
     </div>
   )
 }
