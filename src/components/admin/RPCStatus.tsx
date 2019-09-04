08be0edaf6cbf03f8157f688dfa73e99040f7e90
lrypto <lloydrebello@gmail.com>
2019-09-04 11:40:15 -0400

    componet for status integrated into rpc with a interval

--- src/components/admin/RPCStatus.tsx
+++ src/components/admin/RPCStatus.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Card } from 'antd';
 import { rpcStatus } from '../../common/Actions';
+import { useInterval } from '../../utils/useInterval';
 
 export default function RPCStatus() {
-  const [checkInterval, setCheckInterval] = React.useState(1000);
+  const [checkInterval, setCheckInterval] = React.useState(3000);
   const [rpcIsUp, setRpcIsUp] = React.useState(false);
   const divStyleUp = {color: 'green'};
   const divStyleDown = { color: 'red'};
 
 
-  React.useEffect(() => {
+  useInterval(() => {
     const checkStatus = async() => {
       console.log("checking status...");
       let status: boolean = await rpcStatus();
@@ -17,17 +18,13 @@ export default function RPCStatus() {
     }
 
     checkStatus();
-
-    return () => {
-      //killTimer
-    };
-  }, []);
+  }, checkInterval);
 
 
   return (
     <div className="antDDefault">
       <Card title="RPC Status" bordered={false} >
-        <p>Checking every {checkInterval} ms</p>
+        <p>Checking every: {checkInterval} ms</p>
         <div>Status: {rpcIsUp ?
             <span style={divStyleUp}>UP</span> :
             <span style={divStyleDown}>DOWN!</span>
