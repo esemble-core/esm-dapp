40b2c19fca518892d86a70266268462324946cb1
lrypto <lloydrebello@gmail.com>
2019-09-05 08:15:54 -0400

    added spinner to rpc status component

--- src/components/admin/RPCStatus.tsx
+++ src/components/admin/RPCStatus.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Card } from 'antd';
+import { Card, Spin } from 'antd';
 import { rpcStatus } from '../../common/Actions';
 import { useInterval } from '../../utils/useInterval';
 
 export default function RPCStatus() {
   const [checkInterval, setCheckInterval] = React.useState(3000);
   const [rpcIsUp, setRpcIsUp] = React.useState(false);
+  const [showSpinner, setShowSpinner] = React.useState(false);
   const divStyleUp = {color: 'green'};
   const divStyleDown = { color: 'red'};
 
@@ -15,7 +16,12 @@ export default function RPCStatus() {
       console.log("checking status...");
       let status: boolean = await rpcStatus();
       setRpcIsUp(status);
+
+      
     }
+
+    setShowSpinner(true);
+    setTimeout(function() { setShowSpinner(false); }, 750);
 
     checkStatus();
   }, checkInterval);
@@ -24,7 +30,8 @@ export default function RPCStatus() {
   return (
     <div className="antDDefault">
       <Card title="RPC Status" bordered={false} >
-        <p>Checking every: {checkInterval} ms</p>
+        <div>Checking every: {checkInterval} ms {showSpinner ? <Spin size="small" /> : ''}
+        </div>
         <div>Status: {rpcIsUp ?
             <span style={divStyleUp}>UP</span> :
             <span style={divStyleDown}>DOWN!</span>
