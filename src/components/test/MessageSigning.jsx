3d313abcb4bc7890efcc57784155add96ef9edbf
lrypto <lloydrebello@gmail.com>
2019-09-11 16:10:45 -0400

    universal-login package added

--- src/components/test/MessageSigning.jsx
+++ src/components/test/MessageSigning.jsx
@@ -32,21 +32,10 @@ export default function MessageSigning() {
                 console.log("signing message:", message);
                 
                 if (web3 !== null && ethAcct){
-                /*  let tempHash = web3.utils.sha3(message);
-                  console.log("sha3 hash for message:", tempHash);
-                 // const acct = web3.eth.accounts[0];
-                 
-                 const accounts = web3.eth.accounts;
-                 console.log("account", accounts);
-                  const tempSig = await web3.eth.sign(message, ethAcct);      
-                 setSignature(tempSig);
-                  console.log("signature for message:", tempSig);
+                  let tempHash = web3.utils.sha3(message);
+                  const tempSig = await web3.eth.personal.sign(tempHash, ethAcct);
+                  setSignature(tempSig);
                   setVerified(false);
-                */
-                //const signed = await web3.eth.sign(message, ethAcct);
-                let tempHash = web3.utils.sha3(message);
-                const tempSig = await web3.eth.personal.sign(tempHash, ethAcct);
-                setSignature(tempSig);
                 } else {
                   console.error("either web3 or the ethAccount are not initilized correctly");
                 }
@@ -68,8 +57,8 @@ export default function MessageSigning() {
             console.log("recover message");
             //let signingAddress = web3.eth.accounts.recover(message, signature);
             let signingAddress = await web3.eth.personal.ecRecover(message, signature);
-            console.log("signing address", signingAddress);
-            console.log("current address", ethAcct);
+            //console.log("signing address", signingAddress);
+            //console.log("current address", ethAcct);
             if (signingAddress===ethAcct){
               setVerified(true);
             }
