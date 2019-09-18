83767662b6440703e744992066109656ec814b64
lrypto <lloydrebello@gmail.com>
2019-09-18 12:45:02 -0400

    integrated portis login

--- src/views/MetaTest.tsx
+++ src/views/MetaTest.tsx
@@ -1,29 +1,57 @@
 import React from 'react';
+import Web3 from 'web3';
 import Portis from '@portis/web3';
 import { Button } from 'antd';
 const PORTIS_DAPP_KEY = '4eebf5aa-26bb-4d6e-b277-7cc52b9628c7';
 const ETH_NETWORK_STR = 'rinkeby';
 
 
-export default function MetaTest() {
-  const portis = new Portis(PORTIS_DAPP_KEY, ETH_NETWORK_STR);
 
+export default function MetaTest() {
+  //const portis = new Portis(PORTIS_DAPP_KEY, ETH_NETWORK_STR);
+  //const web3 = new Web3(portis.provider);
+  const portis = new Portis(PORTIS_DAPP_KEY, ETH_NETWORK_STR)
+  
 
   return (  
   <div className="jumbotron">
   <div className="narrow">
     <div className="col-12">
-      <h3 className="heading text-center">Admin</h3>
+      <h3 className="heading text-center">Meta Tests</h3>
       <div className="heading-underline"></div>
         <React.Fragment>
           <Button
             type="ghost"
-            onClick={()=> {
+            onClick={async()=> {
               console.log("showing portis");
-              portis.showPortis();
+              await portis.showPortis()
+              //const web3 = new Web3(portis.provider);
+
+              portis.onLogin((walletAddress, email, reputation) => {
+                console.log(walletAddress, email, reputation);
+              });
             }}
             >
             Show Portis
+          </Button>
+
+          <Button
+            type="ghost"
+            onClick={()=> {
+              console.log("showing account");
+            }}
+            >
+            See Account
+          </Button>
+
+          <Button
+            type="ghost"
+            onClick={()=> {
+              console.log("logging out");
+              portis.logout();
+            }}
+            >
+            Logout
           </Button>
         </React.Fragment>
         
@@ -33,10 +61,3 @@ export default function MetaTest() {
    
   )
 }
-
-
-/*
-
-"web3": "1.0.0-beta.34",
-
-*/
